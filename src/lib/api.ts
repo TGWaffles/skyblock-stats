7782446b068ad1a67948102696fa9d54cc90de33
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2024-10-18 14:57:52 +0100

    automatically set api url based on env vars

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 // the trailing slash is required
 // export const API_URL = 'https://skyblock-api.matdoes.dev/'
-export const API_URL = 'https://statsapi.tem.cx/'
-// export const API_URL = 'http://localhost:8080/'
+export const API_URL = process.env.DEV ? 'http://localhost:8080/' : 'https://statsapi.tem.cx/'
 
 export async function fetchApi(path: string, fetch: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response>, init?: RequestInit | undefined) {
 	// the header is set in hooks.server.ts
