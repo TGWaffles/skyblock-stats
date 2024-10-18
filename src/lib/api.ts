5a0ce4d7326cf2c18f535407ca9b2fdf434ed52a
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2024-10-18 16:58:03 +0100

    use vite envs to switch prod/dev environments for api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 // the trailing slash is required
 // export const API_URL = 'https://skyblock-api.matdoes.dev/'
-export const API_URL = process.env.DEV ? 'http://localhost:8080/' : 'https://statsapi.tem.cx/'
+export const API_URL = import.meta.env.DEV ? 'http://localhost:8080/' : 'https://statsapi.tem.cx/'
 
 export async function fetchApi(path: string, fetch: (info: RequestInfo, init?: RequestInit | undefined) => Promise<Response>, init?: RequestInit | undefined) {
 	// the header is set in hooks.server.ts
